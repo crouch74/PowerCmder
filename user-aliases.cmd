b9932682be87b6cfe9aa915dc2d2d6fdb8f31ce8
Ahmed Eid <ahmedaeid@outlook.com>
2017-07-11 13:19:16 +0200

    fix

--- user-aliases.cmd
+++ user-aliases.cmd
@@ -33,8 +33,7 @@ gld=git log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]
 glg=git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)" --all
 glg2=git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)" --all
 gfl=git log -u
-ggr=git grep -Ii
-gcp=git cherry-pick
+gcp=git cherry-pick $1
 gs=git status
 gst=git status -sb
 gc=git commit
